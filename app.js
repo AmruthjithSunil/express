19ca8bed678a557e7f962580f5d31812b9b1eaf3
AmruthjithSunil <amruthjithsunil@gmail.com>
2023-02-20 17:14:52 +0530

    Starting with express task 21

--- app.js
+++ app.js
@@ -1,33 +1,65 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
+const errorController = require("./controllers/error");
+const sequelize = require("./util/database");
+
+const Product = require("./models/product");
+const User = require("./models/user");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const { Sequelize } = require("sequelize");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminRoutes);
+app.use(async (req, res, next) => {
+  try {
+    req.user = await User.findByPk(1);
+    next();
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize
-    .sync()
-    .then((result) => {
-        //console.log(result);
-        app.listen(3000);
-    })
-    .catch(err => {
-        console.log(err);
-    })
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
+async function syncDb() {
+  try {
+    const result = await sequelize.sync();
+    let user = await User.findByPk(1);
+    if (!user) {
+      user = await User.create({ name: "Amruth", email: "amruth@gmail.com" });
+    }
+    //console.log(user);
+    app.listen(3000);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+syncDb();
+
+// sequelize
+//   .sync({ force: true })
+//   .then((result) => {
+//     //console.log(result);
+//     app.listen(3000);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
--- controllers/admin.js
+++ controllers/admin.js
@@ -1,10 +1,10 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/edit-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    editing: false
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    editing: false,
   });
 };
 
@@ -13,37 +13,42 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product
-    .create({
+  req.user
+    .createProduct({
       title: title,
       price: price,
       imageUrl: imageUrl,
-      description: description
+      description: description,
     })
-    .then(result => {
-      console.log('Created Product');
-      res.redirect('/admin/products')
+    .then((result) => {
+      console.log("Created Product");
+      res.redirect("/admin/products");
     })
-    .catch(err => console.log(err))
+    .catch((err) => console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
   if (!editMode) {
-    return res.redirect('/');
+    return res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId).then(product => {
-    if(!product){
-      return res.redirect('/');
-    }
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
-      editing: true,
-      product: product
-    });
-  }).catch(err => console.log(err));
+  req.user
+    .getProducts({ where: { id: prodId } })
+    //Product.findByPk(prodId)
+    .then((products) => {
+      const product = products[0];
+      if (!product) {
+        return res.redirect("/");
+      }
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
+        editing: true,
+        product: product,
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.postEditProducts = (req, res, next) => {
@@ -53,41 +58,43 @@ exports.postEditProducts = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
   Product.findByPk(prodId)
-    .then(product => {
+    .then((product) => {
       product.title = updatedTitle;
       product.price = updatedPrice;
       product.imageUrl = updatedImageUrl;
       product.description = updatedDesc;
       return product.save();
     })
-    .then(result => {
-      console.log('UPDATED PRODUCT')
-      res.redirect('/admin/products');
+    .then((result) => {
+      console.log("UPDATED PRODUCT");
+      res.redirect("/admin/products");
     })
-    .catch(err => console.log(err))
-}
+    .catch((err) => console.log(err));
+};
 
 exports.getDeleteProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findByPk(prodId)
-    .then(product => {
+    .then((product) => {
       return product.destroy();
     })
-    .then(result => {
-      console.log('DESTROYED PRODUCT')
-      res.redirect('/admin/products');  
+    .then((result) => {
+      console.log("DESTROYED PRODUCT");
+      res.redirect("/admin/products");
     })
     .catch((err) => console.log(err));
-}
+};
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
-    .then(products => {
-      res.render('admin/products', {
+  req.user
+    .getProducts()
+    //Product.findAll()
+    .then((products) => {
+      res.render("admin/products", {
         prods: products,
-        pageTitle: 'Admin Products',
-        path: '/admin/products'
-      })
+        pageTitle: "Admin Products",
+        path: "/admin/products",
+      });
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
